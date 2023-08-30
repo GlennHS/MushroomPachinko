9ace91e5a989937f735d8d48bceeeced548da85b
Glenn Hamilton-Smith <glenniumhs@gmail.com>
2023-08-30 22:36:17 +0100

    Fixes #11.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
     public Canvas creditsCanvas;
+    public TMP_Text dropIndicatorText;
 
     private void Start()
     {
         creditsCanvas.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (GameManager.instance.ballDropping)
+            dropIndicatorText.text = "Drop In Progress!";
+        else
+            dropIndicatorText.text = "Click Here To Drop!";
     }
 
     public void OpenCredits()
